a0626a7d9ff98c546341d7072488062217a5efd9
pmoreno.rodriguez <pmoreno.rodriguez@outlook.com>
2024-07-16 22:33:03 +0200

    Removed the custom.js and custom.css files from the theme

--- future2021.php
+++ future2021.php
@@ -4,11 +4,34 @@ namespace Grav\Theme;
 use Grav\Common\Grav;
 use Grav\Common\Theme;
 
+/**
+ * Future2021 Theme
+ *
+ * Class Future2021
+ *
+ * @category Extensions
+ * @package  Grav\Theme
+ * @author   Pedro Moreno <https://github.com/pmoreno-rodriguez>
+ * @license  http://www.opensource.org/licenses/mit-license.html MIT License
+ * @link     https://github.com/pmoreno-rodriguez/grav-theme-future2021
+ */
+
+
 class Future2021 extends Theme
 {
-    public function onThemeInitialized()
+    public function onTwigSiteVariables ()
     {
+        $themeConfig = $this->config->get('themes.future2021');
 
+        // Add custom.css and custom.js assets if they exists
+
+        if (isset($themeConfig['custom_css']) && $themeConfig['custom_css'] && file_exists(__DIR__ . '/assets/css/custom.css')) {
+            $this->grav['assets']->addCss('theme://assets/css/custom.css', ['priority' => 5]);
+        }
+
+        if (isset($themeConfig['custom_js']) && $themeConfig['custom_js'] && file_exists(__DIR__ . '/assets/js/custom.js')) {
+            $this->grav['assets']->addJs('theme://assets/js/custom.js', ['group' => 'bottom', 'priority' => 15]);
+        }
     }
 
     // Add images to twig template paths to allow inclusion of SVG files
